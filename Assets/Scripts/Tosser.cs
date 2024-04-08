17cec7cbc979c4d1caf814317df68393e05b72c4
Dhruv Sethi <sunny18sethi@gmail.com>
2024-04-08 04:21:32 +0400

    chore: updated player position
    changed players default spawn location

--- Assets/Scripts/Tosser.cs
+++ Assets/Scripts/Tosser.cs
@@ -14,6 +14,89 @@ public class TosserXR : MonoBehaviour
     private List<Vector3> shotsrot = new List<Vector3>(); // List of rotations for the tosser to have
 
     private GameObject lastInstantiatedBall = null; // Reference to the last instantiated ball
+    private bool canLaunch = false; // Flag to determine if the tosser can launch balls
+
+    void Awake()
+    {
+        shotspos.Add(new Vector3(0.004889412f, 0.74f, 1.95f));
+        shotsrot.Add(new Vector3(0f, 180f, 0f));
+        // Add more shot positions and rotations if needed
+    }
+
+    void Start()
+    {
+        // Initialize the timer
+        time = reload;
+    }
+
+    void Update()
+    {
+        // Check if the paddle is being held by either controller before allowing launches
+        canLaunch = IsPaddleHeld();
+
+        // Decrement the timer only if the paddle is held
+        if (canLaunch)
+        {
+            time -= Time.deltaTime;
+        }
+
+        // Check if it's time to toss the ball and if the paddle is held
+        if (time <= 0 && canLaunch)
+        {
+            // Delete the last instantiated ball before creating a new one
+            if (lastInstantiatedBall != null)
+            {
+                Destroy(lastInstantiatedBall);
+            }
+            
+            TossObject();
+            // Reset the timer
+            time = reload;
+        }
+    }
+
+    private void TossObject()
+    {
+        int rand = Random.Range(0, shotspos.Count);
+        transform.position = shotspos[rand];
+        transform.eulerAngles = shotsrot[rand];
+
+        // Instantiate a new ball and keep a reference to it
+        lastInstantiatedBall = Instantiate(ball, transform.position, transform.rotation);
+        lastInstantiatedBall.GetComponent<Rigidbody>().velocity = transform.TransformDirection(Vector3.forward * throwStrength);
+
+        if (launchSound != null)
+        {
+            launchSound.Play();
+        }
+    }
+
+    private bool IsPaddleHeld()
+    {
+        // Check if the paddle is being held by either controller
+        return (transform.childCount > 0); // Assuming the paddle is parented to the controller when held
+    }
+}
+
+
+
+/*
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TosserXR : MonoBehaviour
+{
+    [SerializeField] private GameObject ball; // Reference to the ball prefab
+    [SerializeField] private int reload = 4; // Length of time in between each toss, set a default value
+    public float throwStrength = 10; // Strength of throws, set a default value
+    private float time;
+    public AudioSource launchSound; // Sound that plays every time a ball is tossed
+
+    private List<Vector3> shotspos = new List<Vector3>(); // List of positions for the tosser to be at
+    private List<Vector3> shotsrot = new List<Vector3>(); // List of rotations for the tosser to have
+
+    private GameObject lastInstantiatedBall = null; // Reference to the last instantiated ball
 
     void Awake()
     {
@@ -27,7 +110,7 @@ public class TosserXR : MonoBehaviour
 
         shotspos.Add(new Vector3(2f, 1.1f, -0.5f));
         shotsrot.Add(new Vector3(0f, 0f, 0f));
-        */
+        /
     }
 
     void Start()
@@ -72,3 +155,4 @@ public class TosserXR : MonoBehaviour
         }
     }
 }
+*/
